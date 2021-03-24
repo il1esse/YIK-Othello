50d5be998887ab7d610e0479875ef1145279af40
p1906701 <yann.vincent@etu.univ-lyon1.fr>
2021-03-24 17:39:05 +0100

    modif effet.h et et effet.cpp

--- src/Effet.cpp
+++ src/Effet.cpp
@@ -1,21 +1,140 @@
 #include <effet.h>
 
-effet();
+effet::effet()
+{
+    nomE = "pas attribu�";
+    description = "Rien";
+    special = NULL;
+}
+
+effet::~effet()
 {
 
 }
 
-~effet();
+unsigned char & effet::getNomE() const
+{
+    return nomE;
+}
 
-unsigned char & getNomE() const;
+unsigned char effet::setNomE(char & nouvNomE)
+{
+    nomE = nouvNomE;
+}
+
+unsigned char &  effet::getDescription() const
+{
+    return description
+}
+
+unsigned char effet::setDescription(string & nouvDescription)
+{
+    description = nouvDescription;
+}
+
+unsigned char & effet::getSpecial() const
+{
+    return special
+}
+
+unsigned char effet::setSpecial(bool & nouvSpecial)
+{
+    special = nouvDescription
+}
 
 
-unsigned char setNomE(char & nouvNomE);
 
-unsigned char &  getDescription() const;
 
-unsigned char setDescription(string & nouvDescription);
+HautGauche::HautGauche()
+{
+    nomE = "Haut Gauche";
+    description = "Change l�etat de la case Haut Gauche";
+    special = 0;
+}
+HautGauche::comportementHG(int J.x,int J.y)
+{
+    x = x-1;
+    y = y+1;
+}
 
-unsigned char & getSpecial() const;
 
-unsigned char setSpecial(bool & nouvSpecial);
+HautDroite::HautDroite()
+{
+    nomE = "Haut Droite";
+    description = "Change l�etat de la case Haut Droite";
+    special = 0;
+}
+HautDroite::comportementHD(int J.x,int J.y)
+{
+    x = x+1;
+    y = y+1;
+}
+
+BasGauche::BasGauche()
+{
+    nomE = "BasGauche";
+    description = "Change l�etat de la case BasGauche";
+    special = 0;
+}
+BasGauche::comportementBG(int J.x,int J.y)
+{
+    x = x-1;
+    y = y-1;
+}
+
+BasDroite::BasDroite()
+{
+    nomE = "BasDroite";
+    description = "Change l�etat de la case BasDroite";
+    special = 0;
+}
+BasDroite::comportementBD(int J.x,int J.y)
+{
+    x = x+1;
+    y = y-1;
+}
+
+
+Haut::Haut()
+{
+    nomE = "Haut";
+    description = "Change l�etat de la case Haut";
+    special = 0;
+}
+Haut::comportementH(int J.y)
+{
+    y = y+1;
+}
+
+Droite::Droite()
+{
+    nomE = "Droite";
+    description = "Change l�etat de la case Droite";
+    special = 0;
+}
+Droite::comportementD(int J.x)
+{
+    x = x+1
+}
+
+Gauche::Gauche()
+{
+    nomE = "Gauche";
+    description = "Change l�etat de la case Gauche";
+    special = 0;
+}
+Gauche::comportementG(int J.x)
+{
+    x = x-1
+}
+
+Bas::Bas()
+{
+    nomE = "Bas";
+    description = "Change l�etat de la case Bas";
+    special = 0;
+}
+Bas::comportementB(int J.y)
+{
+    y = y-1;
+}
--- src/Effet.h
+++ src/Effet.h
@@ -5,7 +5,7 @@ class effet{
 
 private :
 
-    char nomE;
+    string nomE;
     string description;
     bool special;
 
@@ -19,13 +19,63 @@ public :
 
     unsigned char setNomE(char & nouvNomE);
 
-    unsigned char &  getDescription() const;
+    unsigned char & getDescription() const;
 
     unsigned char setDescription(string & nouvDescription);
 
     unsigned char & getSpecial() const;
 
     unsigned char setSpecial(bool & nouvSpecial);
+};
+
+class HautGauche : public effet
+{
+    HautGauche();
+    void comportementHG(int J.x,int J.y);
+};
+
+class HautDroite : public effet
+{
+    HautDroite();
+    void comportementHD(int J.x,int J.y);
+};
+
+class BasGauche : public effet
+{
+    BasGauche();
+    void comportementBG(int J.x,int J.y);
+};
+
+class BasDroite : public effet
+{
+    BasDroite();
+    void comportementBD(int J.x,int J.y);
+};
+
+
+
+class Haut : public effet
+{
+    Haut();
+    void comportementH(int J.y);
+};
+
+class Droite : public effet
+{
+    Droite();
+    void comportementD(int J.x);
+};
+
+class Gauche : public effet
+{
+    Gauche();
+    void comportementG(int J.x);
+};
+
+class Bas : public effet
+{
+    Bas();
+    void comportementB(int J.y);
 };
 
 #endif // EFFET_H_INCLUDED

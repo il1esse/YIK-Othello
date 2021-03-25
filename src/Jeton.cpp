736a0e45137b6fecfbb96091132e4d98de7af16b
p1911241 <iliesse.laribi@etu.univ-lyon1.fr>
2021-03-25 15:47:59 +0100

    modification de Jeton.h.cpp / Score.h.cpp

--- src/Jeton.cpp
+++ src/Jeton.cpp
@@ -1,20 +1,6 @@
 /* Inclusion de l'entète du module */
 #include "Jeton.h"
 
-
-
-/* Inclusion d'entêtes standards */
-#include <cassert>
-#include <iostream>
-#include <fstream>
-using namespace std;
-
-
-
-
-
-
-
 /* Définitions des fonctions membres publiques et privées */
 // Constructeur par défaut de la classe: initialise dimx et dimy à 0
 
@@ -24,63 +10,47 @@ using namespace std;
     Jeton::Jeton() {
        NomJeton = "Pion";
        EffetJeton = NULL;
-       Utilisé = false;
+       Utilise = false;
    }
-
-
 
 // Constructeur de la classe Jeton.
     Jeton::Jeton(char nom, Effet effet, bool utilisation) {
        NomJeton = nom;
        EffetJeton = effet;
-       Utilisé = utilisation;
+       Utilise = utilisation;
    }
-
-
 
 // Destructeur de la classe Jeton.
    Jeton::~Jeton () {
        NomJeton = "Pion";
        EffetJeton = NULL;
-       Utilisé = false;
+       Utilise = false;
    }
-
-
 
 // Accesseur : Récupère la valeur NomJeton du Jeton.
    char & Jeton::getNomJeton () const {
       return NomJeton;
    }
 
-
-
 // Accesseur : Récupère la valeur EffetJeton du Jeton.
    Effet & Jeton::getEffet () const {
       return EffetJeton;
    }
-
-
 
 // Accesseur : Récupère la valeur Utilisé du Jeton.
    bool & Jeton::getUtilisation () const {
       return Utilisé;
    }
 
-
-
 // Mutateur : Modifie la valeur NomJeton du Jeton.
    void Jeton::setNomJeton (char & nvNom) {
       NomJeton = nvNom;
    }
 
-
-
 // Mutateur : Modifie l'effet du Jeton.
    void Jeton::setEffet(Effet & nvEffet) {
       EffetJeton = nvEffet;
    }
-
-
 
 // Mutateur : Modifie la valeur Utilisé du Jeton.
    void Jeton::setUtilisation (bool & nvBool) {
--- src/Jeton.h
+++ src/Jeton.h
@@ -1,79 +1,29 @@
 /**
-
-
-
  @brief Jeton contient les données & les entêtes des fonctions jeton.
-
-
-
  @author YIK
-
-
-
  @version 1.0
-
-
-
  @date 2021/03/17
-
-
-
 */
 
 
-
-
-
-
-
 /* Directives pour éviter les inclusions multiples */
-
-
-
 #ifndef JETON_H
-
-
-
 #define JETON_H
 
-
-
-/* Inclusions des entêtes utilisées */
-
-
-
-
-
 /* Définitions de la classes */
-
-
-
 class Jeton {
 /* Données privés */
-
-
  private :
     char NomJeton;
     Effet EffetJeton;
     bool Utilisé;
 
 /* Donnée et fonctions publique */
-
  public :
-
     /**
     @brief Constructeur par défaut de la classe:
     */
-
-
-
     Jeton();
-
-
-
-
-
-
 
     /**
     @brief Constructeur de la classe Jeton.
@@ -83,39 +33,25 @@ class Jeton {
     */
     Jeton(char nom, Effet effet, bool utilisation);
 
-
-
     /**
     @brief Destructeur de la classe Jeton.
     */
     ~Jeton();
-
-
-
-
-
-
 
     /**
     @brief Accesseur : Récupère la valeur NomJeton du Jeton.
     */
     char & getNomJeton() const;
 
-
-
     /**
     @brief Accesseur : Récupère la valeur EffetJeton du Jeton.
     */
     Effet & getEffet() const;
 
-
-
     /**
     @brief Accesseur : Récupère la valeur Utilisé du Jeton.
     */
     bool & getUtilisation() const;
-
-
 
     /**
     @brief Mutateur : Modifie l'effet du Jeton.
@@ -123,31 +59,18 @@ class Jeton {
     */
     void setEffet(Effet & nvEffet);
 
-
-
     /**
     @brief Mutateur : Modifie la valeur Utilisé du Jeton.
     @param nvBool Le nouvelle valeur booléenne d'utilisation Jeton.
     */
     void setUtilisation(bool & nvBool);
 
-
-
-
-
-
-
-
     /**
     @brief Mutateur : Modifie la valeur NomJeton du Jeton.
     @param nvNom Le nouvelle valeur NomJeton du Jeton.
     */
     void setNomJeton(char & nvNom);
-
-
 };
-
-
 
 #endif /* Termine le #ifndef JETON_H */
 

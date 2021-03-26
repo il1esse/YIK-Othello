b7defc87773bf23a56651b32fe8ff25d3e2e69f7
ANDRE-LUBIN KERYANN p1914561 <keryann.andre-lubin@etu.univ-lyon1.fr>
2021-03-26 13:07:31 +0100

    Jeu.cpp incoming

---
+++ src/Jeu.cpp
@@ -0,0 +1,118 @@
+/* Inclusions des entêtes utilisées */
+#include "Jeu.h"
+
+    // Constructeur par défaut de la classe Jeu.
+    Jeu::Jeu(){
+
+    }
+
+
+    // Constructeur de la classe Jeu.
+    Jeu::(Plateau p, Joueur joueur1, Joueur joueur2){
+
+    }
+
+    // Destructeur de la classe Joueur.
+    Jeu::~Jeu(){
+
+    }
+
+
+    // Accesseur : Récupère le plateau de jeu.
+    char & Jeu::getPlateau() const{
+
+    }
+
+    // Accesseur : Récupère le joueur1 de la partie en cours.
+    char & Jeu::getJoueur1() const{
+
+    }
+
+    // Accesseur : Récupère le joueur2 de la partie en cours.
+    char & Jeu::getJoueur2() const{
+
+    }
+
+    // Accesseur : Récupère le score de la partie.
+    unsigned int & Jeu::getScore() const{
+
+    }
+
+
+    // Mutateur : Modifie le plateau de jeu.
+    void Jeu::setPlateau(Plateau & nvPlateau){
+
+    }
+
+    // Mutateur : Modifie le Joueur1.
+    void Jeu::setJoueur1(Joueur & nvJoueur1){
+
+    }
+
+
+    // Mutateur : Modifie le Joueur2.
+    void Jeu::setJoueur2(Joueur & nvJoueur2){
+
+    }
+
+
+    // Mutateur : Modifie le nombre de case possédé par le joueur.
+    void Jeu::setScore(Score & nvScore){
+
+    }
+
+    // Défini les options du jeu.
+    void Jeu::menuJeu(){
+
+    }
+
+    // Met à jour la partie de jeu avec les dernières informations.
+    void Jeu::majJeu(){
+
+    }
+
+    // Met fin à la partie.
+    void Jeu::finDePartie(){
+
+    }
+
+    // Définie aléatoirement l'ordre dans lequelle les joueurs vont jouer.
+    void Jeu::ordreJeu(){
+
+    }
+
+
+    // Définie l'ordre dans lequelle les joueurs vont jouer.
+    void Jeu::ordreJeu(Joueur j){
+
+    }
+
+    // Permet de déterminer le tour des joueurs.
+    void Jeu::joueurTour(){
+
+    }
+
+    // Permet au joueur de jouer son tour.
+    void Jeu::actionJoueur(Joueur j){
+
+    }
+
+    // Permet de fermer le jeu.
+    void Jeu::quitJeu(){
+
+    }
+
+    // Permet de rejouer une partie.
+    void Jeu::rejouerPartie(){
+
+    }
+
+    // Lance le mode Joueur vs Joueur.
+    void Jeu::modeVsJoueur(){
+
+    }
+
+    // Lance le mode vs IA.
+    void Jeu::modeVsIA(){
+
+    }
